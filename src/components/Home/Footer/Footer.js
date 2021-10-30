30210e3957dbebdd96db2c23da0e5583cef11d3f
EASHOVON <ashrafuzzaman.shovon.786@gmail.com>
2021-10-31 02:55:21 +0600

    Header, Banner, Footer and Extra two section added

---
+++ src/components/Home/Footer/Footer.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import AddLocationIcon from '@mui/icons-material/AddLocation';
+import './Footer.css';
+
+const Footer = () =>
+{
+    return (
+        <div className="footer-main">
+            <Container>
+                <div className="row footer-details">
+                    <div className="col-md-4">
+                        <div>
+                            <img src="https://i.ibb.co/PcvzLCV/logo.png" alt="" />
+                        </div>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid maxime aut ut voluptate dolorum nisi ducimus ratione
+                        </p>
+                        <h4>Follow Us:</h4>
+                        <div>
+                            <FacebookIcon /> <InstagramIcon /> <TwitterIcon /> <LinkedInIcon />
+                        </div>
+                    </div>
+                    <div className="col-md-3">
+                        <h3>Contact Us</h3>
+                        <div className="phone d-flex align-items-center">
+                            <div className="me-2">
+                                <LocalPhoneIcon />
+                            </div>
+                            <div className="number">
+                                <span>+8801515226983</span><br />
+                                <span>+8801833921579</span>
+                            </div>
+                        </div>
+                        <div className="d-flex align-items-center">
+                            <div className="me-2">
+                                <DraftsIcon />
+                            </div>
+                            <div className="number">
+                                <span>info@example.com</span><br />
+                                <span>support@example.com</span>
+                            </div>
+                        </div>
+                        <div className="phone d-flex align-items-center">
+                            <div className="me-2">
+                                <AddLocationIcon />
+                            </div>
+                            <div className="number">
+                                <span>2753 Willison Street</span><br />
+                                <span>Eagan, United State</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-2">
+                        <h3>Support</h3>
+                        <p>Contact Us</p>
+                        <p>About Us</p>
+                        <p>Destination</p>
+                        <p>Our Blogs</p>
+                        <p>Package</p>
+                    </div>
+                    <div className="col-md-3">
+                        <h3>We Accepts:</h3>
+                        <div>
+                            <img src="https://i.ibb.co/SvXbhhW/download.png" alt="" />
+                            <img src="https://i.ibb.co/grwfJ8J/download-1.png" alt="" />
+                            <img src="https://i.ibb.co/L0yDDTN/download-2.png" alt="" />
+                            <img src="https://i.ibb.co/t2GhpYq/download-3.png" alt="" />
+                            <img src="https://i.ibb.co/wcWgw4j/download-4.png" alt="" />
+                        </div>
+                    </div>
+                </div>
+                <div className="row py-3">
+                    <p className="text-center">Copyright 2021 <span className="text-warning">TourX</span> | Design By Programming Hero Team</p>
+                </div>
+            </Container>
+        </div>
+    );
+};
+
+export default Footer;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,8 @@
+import { faHandsHelping, faSmile, faStar, faUsers, faUserTie } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Carousel, Container } from 'react-bootstrap';
+import Footer from './Footer/Footer';
 import Header from './Header/Header';
 import './Home.css'
 
@@ -52,6 +55,81 @@ const Home = () =>
                     </Container>
                 </Carousel.Item>
             </Carousel>
+            <Container className="my-5">
+                <h5 className="text-center">Choose your package</h5>
+                <h1 className="text-center">Select Your Best Package <br />For Your Travel</h1>
+            </Container>
+            <Container className="my-5">
+                <h5 className="text-center">Why TourX</h5>
+                <h1 className="text-center">Why You Are Travel With <br />Tourx</h1>
+                <div className="row">
+                    <div className="col-3 card-ex">
+                        <div>
+                            <FontAwesomeIcon className="icon" icon={faUserTie} />
+                        </div>
+                        <div>
+                            <h5>2000+ Our <br />worldwide guide</h5>
+                        </div>
+                    </div>
+                    <div className="col-3 card-ex">
+                        <div>
+                            <FontAwesomeIcon className="icon" icon={faHandsHelping} />
+                        </div>
+                        <div>
+                            <h5>100% trusted travel <br />worldwide guide</h5>
+                        </div>
+                    </div>
+                    <div className="col-3 card-ex">
+                        <div>
+                            <FontAwesomeIcon className="icon" icon={faUsers} />
+                        </div>
+                        <div>
+                            <h5>10+ year of travel <br />experience</h5>
+                        </div>
+                    </div>
+                    <div className="col-3 card-ex">
+                        <div>
+                            <FontAwesomeIcon className="icon" icon={faSmile} />
+                        </div>
+                        <div>
+                            <h5>90% of our traveller <br />happy</h5>
+                        </div>
+                    </div>
+                </div>
+            </Container>
+            <div className="extra-two">
+                <Container className="my-5">
+                    <h5 className="text-center">Feature Tours</h5>
+                    <h1 className="text-center mb-5">See Our Best Popular <br />Destinations</h1>
+                    <div className="row d-flex justify-content-center">
+                        <div className="col-md-5">
+                            <div className="row">
+                                <div className="col-md-4">
+                                    <img className="img-fluid" src="https://tourx-react.egenslab.com/static/media/f-1.7656771a.png" alt="" />
+                                </div>
+                                <div className="col-md-8">
+                                    <h4>Group Travel Go To Bea <br />Amsterdam</h4>
+                                    <small><FontAwesomeIcon className="icon" icon={faStar} /> 7K+Rating</small>
+                                    <h4>$190</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-5">
+                            <div className="row">
+                                <div className="col-md-4">
+                                    <img className="img-fluid" src="https://tourx-react.egenslab.com/static/media/f-1.7656771a.png" alt="" />
+                                </div>
+                                <div className="col-md-8">
+                                    <h4>Group Travel Go To Bea <br />Amsterdam</h4>
+                                    <small><FontAwesomeIcon className="icon" icon={faStar} /> 7K+Rating</small>
+                                    <h4>$190</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
